152d78848cd2d3bb20ceefeb7515cd700b051aef
Abhirup <abhirupp477@gmail.com>
2024-09-09 12:19:53 +0530

    function added for button 2

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const boxresult = document.querySelector('.boxresult');
 btn.addEventListener('click',function(){
     boxresult.innerHTML = '<h2>Great You clicked me</h2>';
 })
+
+btn2.addEventListener('click',function(){
+    boxresult.innerHTML = '<h2>Ahh shit!! I told you not to click.</h2>';
+})
